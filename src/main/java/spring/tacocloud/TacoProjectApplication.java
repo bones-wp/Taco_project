365008d1a7c10b3d6b3bb079e975ab579c48fdc4
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-08-31 14:54:28 +0300

    Initial commit

---
+++ src/main/java/spring/tacocloud/TacoProjectApplication.java
@@ -0,0 +1,13 @@
+package spring.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoProjectApplication.class, args);
+	}
+
+}
