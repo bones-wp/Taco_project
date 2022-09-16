a51dc63b7dfb01ea76f006ae759738d61a93f406
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-09-16 21:25:56 +0300

    5 глава (Spring security) 5.3.1 - OK

--- src/main/java/spring/taco/security/SecurityConfig.java
+++ src/main/java/spring/taco/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package spring.taco.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +14,19 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders").hasRole("USER")
+                .antMatchers("/", "/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .build();
+    }
+
+
 }
